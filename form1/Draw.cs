a328a080ee586ecf31e6a8f8bcb012b2d8f10c9e
JudeShih <jackshih5828@gmail.com>
2015-05-15 18:29:42 +0800

    drawRectangle

---
+++ form1/Draw.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+namespace form1
+{
+    class Draw
+    {
+        private int x= 0;
+        private int y = 0;
+        private int _width;
+        private int _height;
+
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+        
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+        public Draw(int width,int height )
+        { 
+            this.Width = width;
+            this.Height = height;
+        }
+        public void DrawRectangleInt(PaintEventArgs e)
+        {
+
+            
+            Pen blackPen = new Pen(Color.Black, 3);
+
+            e.Graphics.DrawRectangle(blackPen, x, y, _width, _height);
+        }
+    }
+}
---
+++ form1/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace form1
+{
+    public partial class Form1 : Form
+    {
+        private Draw test;
+        public Form1()
+        {
+            int width = int.Parse(textBox1.Text);
+            int length = int.Parse(textBox2.Text);
+            InitializeComponent();
+            Draw test = new Draw(width,length);
+            
+        }
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.test.DrawRectangleInt();
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
